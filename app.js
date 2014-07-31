d04f60a416872cced2c9fc2cd5fa62ecf52a5804
sbokaie <shawn.bokaie@gmail.com>
2014-07-31 11:38:38 -0700

    only really got through the authentication, site map is messed up, need to recover from my file from a couple days back

--- app.js
+++ app.js
@@ -11,41 +11,44 @@ app.set('view engine', 'ejs');
 app.use(methodOvrride());
 app.use(bodyParser.urlencoded());
 app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/public'));
 
-// logging
+
 app.use(function(req, res, next){
   console.log(req.method, req.url)
   next()
 });
 
 app.get('/users', function (req,res) {
-//
+  db.user.findAll().success(function(foundUsers){
+     res.render('users/index', {users: foundUsers });
+     console.log("Here is the post you wanted" + foundPosts)
+  });
 });
 
 app.get('/users/:id', function (req,res) {
   //
 });
 
-app.get('/posts/:id', function (req,res) {
-  var id = req.params.id;
-  //
+// app.get('/posts/:id', function (req,res) {
+//   var id = req.params.id;
+//   //
 
-});
+// });
 
-app.get('/users/:id/posts/new', function(req, res){
-  var id = req.params.id;
-  //
-});
+ app.get('/users/:id/posts/new', function(req, res){
+    res.render('users/new', {userId: req.params.id})
+  });
 
 app.post('/users/:id/posts', function(req, res){
-  var id = req.params.id;
-  //
-});
-
+ var blogTitle = req.body.blogTitle;
+   var blogBody = req.body.blogBody;
+     db.post.create({ title: blogTitle, body: blogBody, userId: req.params.id }).success(function(post){
+       res.redirect('/blog') 
+     })
+  });
 
 
 
 app.listen(3000, function(){
-  console.log("LISTENING ON PORT 3000")
+  console.log("This is Sparta")
 })
--- models/user.js
+++ models/user.js
@@ -1,19 +1,29 @@
 var bcrypt = require("bcrypt");
+//so salty
 var salt = bcrypt.genSaltSync(10);
+//add passport stuff
+var passport = require('passort');
+var passportLocal = require('passport-local');
 
 module.exports = function (sequelize, DataTypes){
    var User = sequelize.define('user', {
      username: { 
         type: DataTypes.STRING, 
+        //all usernames must be unique
         unique: true, 
         validate: {
+          //length parameters
           len: [6, 30],
           }
     },
     password: {
         type:DataTypes.STRING,
         validate: {
+          //cannot leave password empty
           notEmpty: true
+          //not sure if this is okay, but could help validate
+          //length parameter set in newUer ?
+          len:[6, 30],
         }
       }
     },
@@ -22,10 +32,41 @@ module.exports = function (sequelize, DataTypes){
       classMethods: {
         associate: function(db){
           User.hasMany(db.post);
-        }
+      },
+      encryptPassword: function(password) {
+        var hashPass = bcrypt.hashSync(password, salt);
+        return hashPass;
+      },
+      comparePassword: function()
+      //shaky on this part 
+      },
+      newUser: function(username, password, err, success){
+        if(password.length < 8) {
+          err({message: "Your password needs at least 8 characters"});
+        //these messages are objects, need to clarify why
+        } 
+        else({
+          user.create({
+            username: username.
+            password: User.encryptPassword(password)
+          }).error(function(error){
+            //so we can know what the error is
+            console.log(error);
+            if(error.username){
+              err({message:"Username's must be at leat 6 characters"});
+            }
+            else{
+              err({message:"That username already exists, be more creative"});
+            }
+          }).success(function(user){
+            success({message: "You are now a user, we can't wait for you to login"})
+          });
+          //would like to see this entire function in one
+          //line without any of the contents just to see
+          //how its tiered
+        });
       }
     } //close classMethods outer 
-
   ); // close define user
   return User;
 }; // close User function
